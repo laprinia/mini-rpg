f65314eff98535e9bbea3b04ffa5f282ed714e9c
unknown <lavinialoghin7@gmail.com>
2021-01-20 01:34:53 +0200

    added basic navmesh movement

---
+++ Assets/LookAround.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public float rotationSpeed;
+    public Transform targetPlayer;
+    private float yaw;
+
+ 
+
+    void Update()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            {
+                yaw += rotationSpeed * Input.GetAxis("Mouse X");
+                while (yaw < 0f)
+                {
+                    yaw += 360f;
+                }
+                while (yaw >= 360f)
+                {
+                    yaw -= 360f;
+                }
+                targetPlayer.transform.eulerAngles = new Vector3(0, yaw, 0f);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/NavmeshPlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class NavmeshPlayerMovement : MonoBehaviour
+{
+   // public ThirdPersonCharacter character;
+    public NavMeshAgent agent;
+    public Camera mainCamera;
+    public Animator animator;
+
+    private void Start()
+    {
+        // agent.updateRotation = false;
+    }
+    protected bool pathComplete()
+    {
+        if ( Vector3.Distance( agent.destination, agent.transform.position) <= agent.stoppingDistance)
+        {
+            if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray=mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                animator.SetTrigger("isWalkingTrigger");
+                agent.SetDestination(hit.point);
+               
+            }
+        }
+
+        if (pathComplete()) StartCoroutine(StopAnimationCoroutine());
+      
+    }
+
+    IEnumerator StopAnimationCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        animator.SetTrigger("isNotWalkingTrigger");
+        
+    }
+}
