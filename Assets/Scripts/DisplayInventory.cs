c888b393f25cbbba550d04715f32b595a6ef0fc6
unknown <lavinialoghin7@gmail.com>
2021-01-21 15:03:07 +0200

    adding items in inventory works

---
+++ Assets/Scripts/DisplayInventory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayInventory : MonoBehaviour
+{
+    private const int MAX_COUNT = 12;
+    public InventoryObject inventory;
+    public float xStart=208.7f;
+    public float yStart=417.6f;
+    public int xOffset;
+
+    public int noColumns;
+
+    public int yOffset;
+    Dictionary<InventorySlot,GameObject> itemDisplayed=new Dictionary<InventorySlot, GameObject>();
+
+    void Start()
+    {
+        CreateDisplay();
+    }
+    
+    void CreateDisplay()
+    {
+        for (int i= 0; i < inventory.Container.Count; i++)
+        {
+            AddToItemsDisplayed(i);
+        }
+    }
+
+    private void Update()
+    {
+        UpdateDisplay();
+    }
+
+    void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if (itemDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text =
+                    inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                AddToItemsDisplayed(i);
+            }
+        }
+    }
+
+    void AddToItemsDisplayed(int indexInInventory)
+    {
+        int i = indexInInventory;
+        var obj = Instantiate(inventory.Container[i].item.prefab,Vector3.zero, Quaternion.identity, transform);
+        obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+        obj.GetComponent<RectTransform>().transform.position = Vector3.zero;
+        obj.GetComponent<RectTransform>().transform.localPosition =GetPosition(i);
+        obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+        itemDisplayed.Add(inventory.Container[i],obj);
+    }
+    
+    Vector3 GetPosition(int i)
+    {
+        return new Vector3(xStart+(xOffset * (i % noColumns)), yStart+(-yOffset * (i / noColumns)),0f);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
-public class Player : MonoBehaviour
+public class Player: MonoBehaviour
 {
-   
+    public InventoryObject inventory;
+    public NavMeshAgent agent;
+    public Camera mainCamera;
+    public Animator animator;
+    private Vector3 previousPosition;
+    public float curSpeed;
+    float GetCurrentSpeed()
+    {
+        Vector3 curMove = transform.position - previousPosition;
+        curSpeed = curMove.magnitude / Time.deltaTime;
+        previousPosition = transform.position;
+        return curSpeed;
+    }
+
+    private void Update()
+    {
     
-    
-    
+        animator.SetFloat("speed",GetCurrentSpeed());
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray=mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                animator.SetTrigger("isWalkingTrigger");
+                agent.SetDestination(hit.point);
+               
+            }
+        }else if (Input.GetMouseButtonDown(1))
+        {
+            
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+        
+                var item = hit.transform.GetComponent<Item>();
+                if (item)
+                {
+                    inventory.AddItem(item.itemObject, 1);
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
+    }
 }
