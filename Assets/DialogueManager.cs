0e29f03bcfd3dffe64901603f76563da4be1eb75
unknown <lavinialoghin7@gmail.com>
2021-01-26 13:08:38 +0200

    enhanced dialogue system

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -3,9 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    private String button1Text;
+    private String button2Text;
+    public Button firstButton;
+    public Button secondButton;
     public GameObject dialogCanvas;
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
@@ -22,9 +29,21 @@ public class DialogueManager : MonoBehaviour
         sentences=new Queue<string>();
     }
 
-    public void StartDialogue(Dialogue dialogue)
+    public Button getFirstButton()
     {
+        return firstButton;
+    }
+
+    public Button getSecondButton()
+    {
+        return secondButton;
+    }
+    public void StartDialogue(Dialogue dialogue,String button1Text,String button2Text)
+    {
+        this.button1Text = button1Text;
+        this.button2Text = button2Text;
         dialogCanvas.SetActive(true);
+        
         nameText.text = dialogue.name;
         sentences.Clear();
         foreach (var sentence in dialogue.sentences)
@@ -34,10 +53,32 @@ public class DialogueManager : MonoBehaviour
 
         DisplayNextSentence();
     }
+    
+   
 
     public void DisplayNextSentence()
     {
-        if (sentences.Count == 0)
+        if (sentences.Count == 1)
+        {
+            if (button1Text != null)
+            {
+                firstButton.GetComponentInChildren<TextMeshProUGUI>().text = button1Text; 
+            }
+            else
+            {
+                firstButton.gameObject.SetActive(false);
+            }
+
+            if (button2Text != null)
+            {
+                secondButton.GetComponentInChildren<TextMeshProUGUI>().text = button2Text;
+            }
+            else
+            {
+                secondButton.gameObject.SetActive(false);
+            }
+        }
+        else if (sentences.Count == 0)
         {
             EndDialogue();
             return;
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue Dialogue;
+    public Dialogue mainDialogue;
+    public Dialogue questDialogue;
+    public Dialogue conversateDialogue;
+    public Dialogue denyQuestDialogue;
+    public Dialogue acceptQuestDialogue;
 
     public void TriggerDialogue()
     {
-        DialogueManager.instance.StartDialogue(Dialogue);
+        DialogueManager.instance.StartDialogue(mainDialogue,"Ask her about her daughter","Ask about quest");
+        DialogueManager.instance.getFirstButton().onClick.AddListener(onClickConversate);
+        DialogueManager.instance.getSecondButton().onClick.AddListener(onCLickQuest);
+    }
+
+    public void onCLickQuest()
+    {
+       DialogueManager.instance.StartDialogue(questDialogue,"Accept","Deny");
+       DialogueManager.instance.getFirstButton().onClick.AddListener(onClickAccept);
+       DialogueManager.instance.getSecondButton().onClick.AddListener(onClickDeny);
+       
+    }
+
+    public void onClickConversate()
+    {
+       DialogueManager.instance.StartDialogue(conversateDialogue,null,null);
+    }
+    public void onClickAccept()
+    {
+       DialogueManager.instance.StartDialogue(acceptQuestDialogue,null,null);
+       //todo start quest syst
+    }
+
+    public void onClickDeny()
+    {
+       DialogueManager.instance.StartDialogue(denyQuestDialogue,null,null);
     }
 }
